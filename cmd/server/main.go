9ecb4c91edf593cfe077b10843ca24317ef21426
lohit-dev <lohitsaidev@gmail.com>
2026-06-16 22:10:46 +0530

    feat: integrate godotenv for configuration management and add air configuration for live reloading

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,14 @@
-package server
+package main
+
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Failed to load .env", err)
+	}
+}
--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.26.4
 
 require go.uber.org/zap v1.28.0
 
-require go.uber.org/multierr v1.10.0 // indirect
+require (
+	github.com/joho/godotenv v1.5.1
+	go.uber.org/multierr v1.10.0 // indirect
+)
