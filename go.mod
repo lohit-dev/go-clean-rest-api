6234f31353b378f24d1b6d8c639cc7bc3341977d
lohit-dev <lohitsaidev@gmail.com>
2026-06-16 22:21:36 +0530

    feat: implement server initialization with chi router, middleware, and health check endpoint

--- go.mod
+++ go.mod
@@ -2,7 +2,11 @@ module github.com/lohit-dev/go-clean-rest-api
 
 go 1.26.4
 
-require go.uber.org/zap v1.28.0
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/go-chi/chi/v5 v5.3.0
+	go.uber.org/zap v1.28.0
+)
 
 require (
 	github.com/joho/godotenv v1.5.1
--- internal/server.go
+++ internal/server.go
@@ -1,1 +1,44 @@
 package internal
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"go.uber.org/zap"
+)
+
+type Server struct {
+	Port   string
+	Router *chi.Mux
+	Logger *zap.Logger
+}
+
+func New(port string, logger *zap.Logger) *Server {
+	s := &Server{
+		Port:   port,
+		Router: chi.NewRouter(),
+		Logger: logger,
+	}
+
+	s.setupMiddleware()
+	s.setupRoutes()
+
+	return s
+}
+
+func (s *Server) setupMiddleware() {
+	s.Router.Use(middleware.Recoverer)
+	s.Router.Use(middleware.Timeout(60 * time.Second))
+}
+
+func (s *Server) setupRoutes() {
+	s.Router.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(map[string]string{"status": "online"})
+	})
+
+}
